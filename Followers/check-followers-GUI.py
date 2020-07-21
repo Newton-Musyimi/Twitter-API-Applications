d9a64d9eeb23ed4d7297bd20c81425abcbf76c5c
Musyimi <newtonthemaker@gmail.com>
2020-07-21 14:37:28 +0200

    Update check-followers-GUI.py
    Created a getData() function to get user input from the GUI also added code to the main() function for the text labels and text fields the user will utilize while using the app. The program runs continuously until the user manually closes the window

--- Followers/check-followers-GUI.py
+++ Followers/check-followers-GUI.py
@@ -18,6 +18,20 @@ def getExceptionMessage(msg):
 
     return errorMsg
 
+def getData():
+    global label2
+    name = E1.get()
+    if(len(name)<=0):
+        label2.config(text = "No username has been entered!")
+        return
+    try:        
+        followers = api.get_user(str(name)).followers_count
+        followers = name+" has "+str(followers)+" followers."
+        label2.config(text = str(followers))
+    except tweepy.error.TweepError as e:
+        err = str(e.response.text)
+        label2.config(text = (getExceptionMessage(err)))
+
 def main():
     consumer_key = "<Enter your consumer key here>"
     consumer_secret = "<Enter your consumer secret here>"
@@ -31,6 +45,18 @@ def main():
     root = Tk()
     frame = Frame(root)
     frame.pack()
+
+    label1 = Label(frame, text="Username")
+    E1 = Entry(frame, bd = 5, width = 50, justify="center")
+    label2 = Label(frame, text="Number of followers")
+    submit = Button(frame, text ="Submit", command = getData)
+
+    label1.pack()
+    E1.pack()
+    label2.pack()
+    submit.pack(side = BOTTOM)
+    root.title("Twitter Follower Count")
+    root.mainloop()
 
 if __name__ == "__main__":
     main()
